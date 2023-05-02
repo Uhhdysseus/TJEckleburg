f8e7d37d1f05fab13ad33024b84691dea6e2dfb9
uhhdysseus <austin.herrera741@gmail.com>
2023-05-02 15:42:21 -0500

    commit

--- walkietalkie.py
+++ walkietalkie.py
@@ -1,7 +1,7 @@
 import serial
 import time
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
         ser.write(b"Hello from Raspberry Pi!\n")
