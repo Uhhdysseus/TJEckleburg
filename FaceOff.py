0bdaee7f6a363ba439c03cfbc39f82e5ba79c835
uhhdysseus <austin.herrera741@gmail.com>
2023-05-12 15:04:01 -0500

    blah

--- FaceOff.py
+++ FaceOff.py
@@ -97,7 +97,7 @@ def readPI():
 def walkietalkie(command):
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
-    ser.write(command)
+    ser.write(b"%s\n",command)
     try:
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
